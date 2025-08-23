348e668e3ebac5879b7fb88202bf11e99ddaaba9
sijun <sionwer5@gmail.com>
2025-08-23 13:50:26 +0900

    feat: app h1 수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,8 +13,12 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "썸출장마사지 | 전원 20대 관리사 | 100% 후불제 | 서울 경기 인천",
-  description: "서울·경기·인천 30분 내 도착. 365일 24시간 영업하고있습니다. 현금·계좌이체·카드결제 가능합니다.",
+    title: "썸출장마사지 | 전원 20대 관리사 | 100% 후불제 | 서울 경기 인천",
+    description: "서울·경기·인천 30분 내 도착. 365일 24시간 영업하고있습니다. 현금·계좌이체·카드결제 가능합니다.",
+    robots: {
+        index: true,
+        follow: true,
+    },
 };
 
 export default function RootLayout({
